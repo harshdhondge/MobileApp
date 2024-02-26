718a231087a8a46d65c8350797c04a5404094e00
Harshad Dhondge <102278515+harshdhondge@users.noreply.github.com>
2024-02-26 17:20:19 +0530

    frontend
    frontend of web app and firebase is setuped

---
+++ mobileJS/index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://mobileapp-f44d1-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const ShoppingListInDB = ref(database, "ShoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(ShoppingListInDB, inputValue)
+    
+    console.log(`${inputValue} added to database`)
+})
