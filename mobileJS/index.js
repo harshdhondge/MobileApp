a8e573fe7104795c104451b3e8fcb3c89b81819e
Harshad Dhondge <102278515+harshdhondge@users.noreply.github.com>
2024-02-27 17:46:13 +0530

    backend done

--- mobileJS/index.js
+++ mobileJS/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://mobileapp-f44d1-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -7,15 +7,38 @@ const appSettings = {
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const ShoppingListInDB = ref(database, "ShoppingList")
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    push(ShoppingListInDB, inputValue)
+    push(shoppingListInDB, inputValue)
     
-    console.log(`${inputValue} added to database`)
+    clearInputFieldEl()
 })
+
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.entries(snapshot.val())
+    clearshoppingList()
+    for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i]
+        let currentItemID = currentItem[0]
+        let currentItemValue = currentItem[1]
+        appendItemToShoppingListEl(currentItemValue)
+    }
+})
+
+function clearshoppingList(){
+    shoppingListEl.innerHTML = ""
+}
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
